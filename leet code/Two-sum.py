116c969c61a0acb113776dbfae3374cce0cc6b55
何彥南 <46644821+aaron1aaron2@users.noreply.github.com>
2019-09-30 12:56:13 +0800

    Update Two-sum.py

--- leet code/Two-sum.py
+++ leet code/Two-sum.py
@@ -1,9 +1,10 @@
 class Solution(object):
-	def twoSum(self, l, target):
-		complement = dict()
-		for i in range(0, len(l)):
-			cmp = target - l[i]
-			if l[i] in complement:
-				return [complement[l[i]], i]
+	def twoSum(self, List, target):
+		complement = dict() # key: list 中每個數字跟目標的差， value: 對應差的index
+		for i in range(0, len(List)):
+			cmp = target - List[i] 
+			# 只要list 裡的數字有在 complement 的 key 裡，則組成配對返回對應的index。
+			if List[i] in complement:
+				return [complement[List[i]], i]
 			complement[cmp] = i
 		return None
