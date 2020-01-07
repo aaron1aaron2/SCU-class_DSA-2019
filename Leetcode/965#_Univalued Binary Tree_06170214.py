e196af68ad301f9d9b75c1056e080b3dfead659f
何彥南 <46644821+aaron1aaron2@users.noreply.github.com>
2020-01-07 23:45:59 +0800

    Create 965#_Univalued Binary Tree_06170214.py

---
+++ Leetcode/965#_Univalued Binary Tree_06170214.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isUnivalTree(self, root: TreeNode) -> bool:
+        
+        if root == None:
+            return True
+        if root.left!=None and root.val != root.left.val:
+            return False
+        if root.right!=None and root.val != root.right.val:
+            return False
+        
+        return self.isUnivalTree(root.left) and self.isUnivalTree(root.right)
